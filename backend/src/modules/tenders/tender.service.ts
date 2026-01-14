17c78f38d4fc7a6babe7ecaefd25a55abddec04b
Ricky004 <tridipdam001@gmail.com>
2026-01-14 17:43:21 +0530

    chore(build): Update build process and fix import paths

--- backend/src/modules/tenders/tender.service.ts
+++ backend/src/modules/tenders/tender.service.ts
@@ -1,4 +1,4 @@
-import { prisma } from '../../lib/prisma';
+import { prisma } from '@/lib/prisma';
 import { TenderType, TenderStatus, TenderDocumentType, DocumentStatus, EMDStatus, TenderAuditAction } from '@/generated/prisma';
 
 export class TenderService {
